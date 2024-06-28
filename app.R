0da48564bae43911a6173d438c5c74ea37a9efb5
Lance Upton <lance.upton@gmail.com>
2024-06-27 21:17:10 -0400

    Update demo name.

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 
 pkgload::load_all()
 app_run(
-  title = "shiny.reviewer Demo Application",
+  title = "shiny.reviewer Demo",
   content_meta = CONTENT_META_EXAMPLE,
   reviews = REVIEWS_EXAMPLE, 
   group_meta = GROUP_META_EXAMPLE,
